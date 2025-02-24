a32c2cabbb67370f65032742f172a59f046c620a
Leonardo Cunha <leo.francasp97@gmail.com>
2025-02-24 20:09:39 -0300

    Princípios de autenticação JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
+import { profile } from './controllers/profile';
 
 export async function AppRoutes(app: FastifyInstance) {
   app.post('/users', register);
 
   app.post('/sessions', authenticate);
+
+  // ROTAS QUANDO O USUÁRIO ESTIVER AUTENTICADO
+  app.get('/me', profile);
 }
