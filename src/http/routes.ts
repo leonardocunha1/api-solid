c3817941db579c497c1192a166ab1063e81c45fb
Leonardo Cunha <leo.francasp97@gmail.com>
2025-02-25 19:52:52 -0300

    Adicionando middleware e fazendo o E2E do profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
 import { profile } from './controllers/profile';
+import { verifyJwt } from '@/middlewares/verify-jwt';
 
 export async function AppRoutes(app: FastifyInstance) {
   app.post('/users', register);
@@ -9,5 +10,11 @@ export async function AppRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate);
 
   // ROTAS QUANDO O USUÁRIO ESTIVER AUTENTICADO
-  app.get('/me', profile);
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJwt],
+    },
+    profile,
+  );
 }
