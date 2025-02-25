a3437a618e0d4450d0f70cbef2aa31384b5cb320
Leonardo Cunha <leo.francasp97@gmail.com>
2025-02-25 19:29:09 -0300

    Teste E2E da autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
 
-test('ok', () => {});
+describe('Authenticate (2e2)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Leonardo Oliveira',
+      email: 'leonardo.oliveira@hotmail.com',
+      password: '123456',
+    });
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'leonardo.oliveira@hotmail.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
