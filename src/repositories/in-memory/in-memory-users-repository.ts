a39af4c84f48167db89778c177af67912a07f750
Leonardo Cunha <leo.francasp97@gmail.com>
2025-03-08 14:40:37 -0300

    Fix: tipagem do create no in-memory-users

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,4 +1,4 @@
-import { User, Prisma } from '@prisma/client';
+import { User, Prisma, Role } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
 import { randomUUID } from 'node:crypto';
 
@@ -33,6 +33,7 @@ export class InMemoryUsersRepository implements UsersRepository {
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
+      role: data.role as Role,
       created_at: new Date(),
     };
 
